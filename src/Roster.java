0613e21bf9be494a542065f826908ad81497ad6e
Navneeth Jayendran <jayndrn2@illinois.edu>
2015-12-21 00:58:38 -0600

    Update Roster.java
    Fixed indentation to be more consistent and fixed naming of OutOfBoundsException to IndexOutOfBoundsException.

--- src/Roster.java
+++ src/Roster.java
@@ -9,28 +9,28 @@
  */
 public class Roster{
 
-    private Student[] map;
-    private int numStudents;
+	private Student[] map;
+	private int numStudents;
 	
-    /**
-     * Constructor for the Roster class. Creates an array of specified capacity
-     * and fills it up with new Student objects. The Students are initialized
-     * based off the ID codes in the NRList argument.
-     */
-    public Roster(NRList codes, int capacity){
-        map = new Student[capacity];
-	    /* Goes through all the ID codes in the NRList argument and creates a
-	     * Student object for each one. The Students created are stored in an
-	     * array at the index corresponding to their ID codes. So map[154] would
-	     * correspond to the Student whose ID code is 154. That being said, we
-	     * should ensure that the range of Student ID codes is relatively small
-	     * or else the array will waste space.
-	     */
-        for (NetIDPair elem : codes){ //NRList needs a functionality like this
-            map[elem.getID()] = new Student(elem.getID());
-            numStudents = codes.size(); //NRList needs a functionality like this
-        }
-    }
+	/**
+	 * Constructor for the Roster class. Creates an array of specified capacity
+	 * and fills it up with new Student objects. The Students are initialized
+	 * based off the ID codes in the NRList argument.
+	 */
+	public Roster(NRList codes, int capacity){
+		map = new Student[capacity];
+		/* Goes through all the ID codes in the NRList argument and creates a
+		 * Student object for each one. The Students created are stored in an
+		 * array at the index corresponding to their ID codes. So map[154] would
+		 * correspond to the Student whose ID code is 154. That being said, we
+		 * should ensure that the range of Student ID codes is relatively small
+		 * or else the array will waste space.
+		 */
+		for (NetIDPair elem : codes){ //NRList needs a functionality like this
+			map[elem.getID()] = new Student(elem.getID());
+			numStudents = codes.size(); //NRList needs a functionality like this
+		}
+	}
 
 	/**
 	 * Given an ID, returns the Student with the matching ID in the Roster. 
@@ -58,16 +58,16 @@ public class Roster{
 	 * Roster. Ensure extra space when constructing Roster or else this will
 	 * not be possible.
 	 */
-    public void addStudent(int ID){
-        if (ID < 0 || ID >= map.size())
-            throw new OutOfBoundsException(ID + " is out of Roster Range.");
-        if (map[ID] != null)
-            throw new IllegalArgumentException(ID + " is already taken.");
-        map[ID] = new Student(ID);
-        ++numStudents;
+	public void addStudent(int ID){
+		if (ID < 0 || ID >= map.size())
+			throw new IndexOutOfBoundsException(ID + " is out of Roster Range.");
+		if (map[ID] != null)
+			throw new IllegalArgumentException(ID + " is already taken.");
+	map[ID] = new Student(ID);
+	++numStudents;
 	}
 	 
-    /**
+	/**
 	 * Resizes this Roster to allow addition of more Students. If shrinking
 	 * the Roster would cut off existing students, throws an exception.
 	 *
@@ -77,5 +77,5 @@ public class Roster{
 	/*
 	 * @todo Implement
 	 */
-    }
+	}
 }
