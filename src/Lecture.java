bae946451fd4c00a56cd6ca3c1b2bbdb748c6d16
Navneeth Jayendran <jayndrn2@illinois.edu>
2015-12-21 05:37:21 -0600

    Update Lecture.java
    I changed getDate(void) to return a deep copy of the Date member variable to ensure immutability, and I documented all the methods. I also removed the Lecture(Date, FeedbackEntry) constructor because it is not useful to the extent that we would ever want to use it anywhere in our code. Such a constructor would make sense if we frequently had to make Lectures containing only a single FeedbackEntry, but that's not the case. Renamed member variable "ID" and method "getValues" to the more expressive "lectureNumber(void)" and "getRatingDistribution(void)",.

--- src/Lecture.java
+++ src/Lecture.java
@@ -6,46 +6,76 @@ import java.util.Date;
 public class Lecture {
 
 	private static int AUTO_INCREMENT = 0;
-	private int ID; //TODO: Discuss this name
+	private int lectureNumber; //TODO: Discuss this name
 	public ArrayList<FeedbackEntry> recordsByTime;
 	private Date date;
 	private String[] topics;// OR ArrayList<String>
 	
-	///CONSTRUCTORS
-	//Constructor that is always called by other constructors
+	/**
+	 * Base constructor called by all other constructors.
+	 * Assigns a Date to this Lecture object.
+	 * 
+	 * @param d The time this lecture began.
+	 */
 	public Lecture(Date d){
-		ID = AUTO_INCREMENT++;
+		lectureNumber = AUTO_INCREMENT++;
 		recordsByTime = new ArrayList<>();
 		date = d;
 	}
 	
+	/**
+	 * Constructor with argument to assign the topics for
+	 * the Lecture object.
+	 * 
+	 * @param d     The time this lecture began.
+	 * @param topic A list of topics discussed during this lecture.
+	 */
 	public Lecture(Date d, String[] topics){
 		this(d);
 		this.topics = topics;
 	}
 	
-	public Lecture(Date d, FeedbackEntry entry){
-		this(d);
-		this.add(entry);
-	}
-	
-	///GETTERS/SETTERS
+	/**
+	 * @return A copy of this Lecture's date.
+	 */
 	public Date getDate(){
-		return date;
+		return new Date(date.getTime());
 	}
 	
+	/**
+	 * This method adds a FeedbackEntry to this Lecture. As a 
+	 * precondition, entries should be added in chronological 
+	 * order.
+	 * 
+	 * @param entry The entry to add to this Lecture. This entry should
+	 *              come after the last entry added to the Lecture.
+	 */
 	public void add(FeedbackEntry entry){
 		recordsByTime.add(entry);
 	}
 	
-	public int[] getValues() {
+	/**
+	 * Method that returns the unadjusted rating distribution of all
+	 * FeedbackEntries for this Lecture. The 0th element of this
+	 * array corresponds to the number of entries with ratings of
+	 * 1, and the 9th to the number of entries with ratings of 10.
+	 * 
+	 * @return An array of ints in which arr[idx] represents the number
+	 *         of FeedbackEntries with ratings of (idx+1) in this Lecture
+	 */
+	public int[] getRatingDistribution() {
 		int[] values = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 		for (FeedbackEntry element : recordsByTime)
 			values[element.getGrade() - 1]++;
 		return values;
 	}
 	
-	///ANALYSIS
+	/**
+	 * Calculates and returns the unadjusted average rating given by
+	 * all FeedbackEntries for this lecture.
+	 * 
+	 * @return The average rating of all FeedbackEntries in this Lecture
+	 */
 	public double getAverage(){
 		if (recordsByTime.isEmpty())
 			return -1;
@@ -55,6 +85,13 @@ public class Lecture {
 		return sum/recordsByTime.size();
 	}
 	
+	/**
+	 * Calculates and returns the unadjusted standard deviation of ratings
+	 * given by all FeedbackEntries for this lecture.
+	 * 
+	 * @return The rating standard deviation of all FeedbackEntries in this 
+	 *         Lecture
+	 */
 	public double getStdDev(){
 		if (recordsByTime.isEmpty())
 			return -1;
@@ -65,7 +102,10 @@ public class Lecture {
 		return Math.sqrt(devSq/recordsByTime.size());
 	}
 	
-	@Override
+	/**
+	 * @return A string in the form "ID: %, Date: %, Number of Entries: %,"
+	 *         "Mean: %, Standard Deviation: %"
+	 */
 	public String toString(){
 		return String.format("ID: %d\n\tDate: ", ID) + date + String.format("\n\tNumber of Entries: %d\n\t"
 				+ "Mean: %f\n\tStandard Deviation: %f", recordsByTime.size(), getAverage(), getStdDev());		
