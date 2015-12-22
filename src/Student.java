880932b110c761d0200875345cd93129539c632a
Navneeth Jayendran <jayndrn2@illinois.edu>
2015-12-21 18:56:44 -0800

    Update Student.java
    Added various method stubs for later implementation with documentation. Note: fix formatting by replacing spaces with tabs and correcting the indent level.

--- src/Student.java
+++ src/Student.java
@@ -12,7 +12,10 @@ public class Student{
 	private int ID;
 	private boolean female; //One possibility for what we could store here
 	private ArrayList<FeedbackEntry> feedbackGiven;
- 
+        private double weight;
+        
+
+
     /**
      * Static method that uses a right-biased binary search of
      * existing Lectures to associate a FeedbackEntry with a Lecture. 
@@ -29,7 +32,7 @@ public class Student{
         int hi = lecs.size()-1;
         Date search = key.getDate();
         while (hi != lo){
-            int mid = (lo + hi)/2;
+            int mid = (lo + hi + 1)/2;
             Date comp = lecs.get(mid).getDate();
             switch (search.compareTo(comp)){
             case -1:
@@ -49,4 +52,69 @@ public class Student{
 	public Student(int code){
 		ID = code;
 	}
+
+        /**
+         * Computes and returns the weight of this
+         * Student, used when calculating weighted
+         * grade distributions for a Lecture. 
+         * @todo Insert implementation.
+         *
+         * @return The weight given to this Student's feedback.
+         */
+        public double getWeight(){
+            return 1;
+        }
+
+        /**
+         * Add a FeedbackEntry to this Student's record thereof.
+         * This method imposes a chronologial precondition.
+         * @todo Implement.
+         *
+         * @param entry Feedback made by this Student
+         */
+        public void addEntry(FeedbackEntry entry){
+            return;
+        }
+
+        /**
+         * @return The ID code of this student.
+         */
+        public int getID(){
+           return ID;
+        }
+
+        /**
+         * Setter for ID (is this really useful?)
+         */
+        public void setID(int code){
+           ID = code;
+        }
+
+        /**
+         * @return Number of valid feedback data points given by this Student.
+         * @todo Implement.
+         */
+        public int feedbackGiven(){
+            return -1;
+        }
+
+        /**
+         * @return The average rating given by this Student for all
+         *         lectures so far in the semester.
+         * @todo Implement if needed.
+         */
+        public double ratingMean(){
+            return -1;
+        }
+
+        /**
+         * @return The standard deviation of all ratings given by
+         *         this Student throughout the semester
+         * @todo Implement if needed.
+         */
+        public double ratingStdDev(){
+            return -1;
+        }
+
+        
 }
