bcbc9219a754497c554526cd24975403153728e4
Navneeth Jayendran <jayndrn2@illinois.edu>
2015-12-21 17:34:38 -0800

    Update Student.java
    Added a potential FeedbackEntry-to-Lecture map. Though it seems to be most logically a static member of the LectureInitializer class, since the association between a FeedbackEntry and a Lecture is independent of the Student who made the entry.

--- src/Student.java
+++ src/Student.java
@@ -13,6 +13,36 @@ public class Student{
 	private boolean female; //One possibility for what we could store here
 	private ArrayList<FeedbackEntry> feedbackGiven;
  
+    /**
+     * Static method that uses a right-biased binary search of
+     * existing Lectures to associate a FeedbackEntry with a Lecture. 
+     * This method may be better in another class. Runs in logarithmic time
+     * with respect to the number of Lectures.
+     *
+     * @param key The entry to be associated with a Lecture.
+     *
+     * @return The Lecture corresponding to an entry.
+     */
+    public static Lecture get(FeedbackEntry key){
+        ArrayList<Lecture> lecs = LectureInitializer.lectures;
+        int lo = 0;
+        int hi = lecs.size()-1;
+        Date search = key.getDate();
+        while (hi != lo){
+            int mid = (lo + hi)/2;
+            Date comp = lecs.get(mid).getDate();
+            switch (search.compareTo(comp)){
+            case -1:
+                hi = mid-1; break;
+            case 1:
+                lo = mid; break;
+            default:
+                return lecs.get(mid);
+            }
+        }
+        return lecs.get(mid);
+    }
+
  	/**
  	 * Ctor. Needs work.
  	 */
