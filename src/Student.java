ec955a57d0ac2dc5dc25770f3b1249cd520a3892
Pranay Prakash <pranay.gp@gmail.com>
2015-12-23 14:52:55 +0400

    Moved _static_  function from  to

--- src/Student.java
+++ src/Student.java
@@ -13,37 +13,7 @@ public class Student{
 	//private boolean female; //One possibility for what we could store here
 	private ArrayList<FeedbackEntry> records;
 	private double weight;
-        
-
-	/**
-	 * Static method that uses a right-biased binary search of existing Lectures
-	 * to associate a FeedbackEntry with a Lecture. This method may be better in
-	 * another class. Runs in logarithmic time with respect to the number of 
-	 * Lectures.
-	 *
-	 * @param key The entry to be associated with a Lecture.
-	 *
-	 * @return The Lecture corresponding to an entry.
-	 */
-	public static Lecture get(FeedbackEntry key){
-		ArrayList<Lecture> lecs = LectureInitializer.lectures;
-		int lo = 0;
-		int hi = lecs.size()-1;
-		Date search = key.getDate();
-		while (hi != lo){
-			int mid = (lo + hi + 1)/2;
-			Date comp = lecs.get(mid).getDate();
-			switch (search.compareTo(comp)){
-			case -1:
-				hi = mid-1; break; //Can exclude Dates that come after target
-			case 1:
-				lo = mid; break;  //Can't exclude Dates that come before
-			default:
-				return lecs.get(mid); //Unlikely case of exact match
-			}
-		}
-		return lecs.get(lo);
-	}
+ 
 
  	/**
  	 * ID Ctor. Needs work?
