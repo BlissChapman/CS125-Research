0594d22022137870c563044338210b0514837e41
Pranay Prakash <pranay.gp@gmail.com>
2015-12-31 01:18:35 +0400

    Restructuring

---
+++ src/MainTest.java
@@ -0,0 +1,19 @@
+import junit.framework.TestCase;
+
+/**
+* @author BlissChapman
+*/
+public class MainTest extends TestCase {
+	
+	public void tearDown() {
+	}
+	public void setUp() throws Exception{
+		CheckInputOutput.setUp();
+		super.setUp();
+	}
+	
+	public void testBuildRanCompletely() {
+		Main.main(new String[0]);
+		CheckInputOutput.checkOutputContains("ANALYSIS COMPLETE");
+	}
+}
