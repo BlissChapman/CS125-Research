2c52d4e7484ed9e26a66d7427142395a809ee1c9
Navneeth Jayendran <jayndrn2@illinois.edu>
2016-01-02 22:47:03 -0600

    Adding caching, case handling, more
    1: Added caching of certain variables that are computationally heavy to compute and might be computed in quick succession in a graphing algorithm. This has to be tested, but it's fairly simple. See the comments on the cached variables and refreshCache() for implementation details.
    2: implemented Iterable<PeerInteraction> interface
    3: Added a pushBack() method that allows PeerInteractions to be inserted in unsorted order (just to weaken preconditions and the insidious bugs that can arise from breaking them)
    4: Added a mergeAllDuplicates() and a mergeRecentDuplications() method [I said I'd do this a while ago but never got to it]. I'm not sure which to keep right now. The former is much safer but can be unnecessarily slower depending on usage patterns. The reason I don't run these in the addEntry() method is that merging entries sequentially will have unwanted formatting effects (see PeerInteraction(PeerInteraction[]) for details).They are meant to be merged as batches.
    5: Added a test main method. This currently tests merging, inserting unsorted data, and throwing exceptions for ID mismatches.

--- src/Student.java
+++ src/Student.java
@@ -1,31 +1,40 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.RandomAccess;
+import java.util.Scanner;
 
 /**
- * Important: Student class. Stores information regarding a particular Student in class, 
- * including their lecture attendance and possibly grades. Each Student should probably
- * contain all the PeerInteractions made by the student.
+ * Student class. Stores information regarding a particular Student in class, 
+ * including their lecture attendance and possibly grades. Each Student
+ * will contain all the proper PeerInteractions made by the student.
  *
  * @author CS125 Research
- * @todo Implement everything
+ * @todo Implement more.
  */
-class Student{
+class Student implements Iterable<PeerInteraction>{
 	public static final float WEIGHT_PROPORTIONALITY_CONSTANT = 1;
 	public static final float WEIGHT_THRESHOLD = 10;
 	
-	/**
-	 * ID of this Student. This value is immutable.
-	 */
+	/* ID of this Student. This value is immutable. */
 	private final int ID;
+	
 	//private boolean female; //One possibility for what we could store here
 	
-	/**
-	 * The collection of all PeerInteractions made by the Student, sorted
-	 * in chronological order.
-	 */
+	/* The collection of all PeerInteractions made by the Student, sorted
+	 * in chronological order. */
 	private ArrayList<PeerInteraction> records;
-	
-	private double weight = Double.NaN;
-	
+
+	/* Caching variables for several computationally nontrivial methods.
+	 * Stores results of these method calls and is recomputed whenever
+	 * the Student is mutated. */
+	private float weight = Float.NaN;
+	private float ratingMean = Float.NaN;
+	private float ratingStdDev = Float.NaN;
+	/* When the cache is to be refreshed, this boolean is set true. Any
+	 * methods that use the above variables will recompute values when
+	 * this boolean is set. See refreshCache()*/
+	private boolean mutated = false;
 	
  	/**
  	 * ID Ctor. Needs work?
@@ -45,6 +54,7 @@ class Student{
 	public Student(PeerInteraction entry){
 		this(entry.getPersonID());
 		records.add(entry);
+		refreshCache();
 	}
 
 	/**
@@ -57,6 +67,8 @@ class Student{
 	 * @return The weight given to this Student's feedback.
 	 */
 	public double feedbackWeight(){
+		if (!mutated)
+			return WEIGHT_PROPORTIONALITY_CONSTANT*weight;
 		if (records.size() == 0)
 			return Double.NaN;
 		int numberOfCommonResponses = 0; // For now, if the feedback was 5 or 10, it's not important
@@ -78,17 +90,19 @@ class Student{
 
 	/**
 	 * Add a PeerInteraction to this Student's record thereof. This method 
-	 * imposes a chronologial precondition. Throws an exception if an
-	 * entry passed in has a personID which differs from this.ID .
+	 * will sort the record by date after insertion. Throws an exception 
+	 * if an entry passed in has a personID which differs from this.ID .
 	 *
 	 * @param entry PeerInteraction made by this Student
 	 */
 	public void addEntry(PeerInteraction entry){
 		if (entry.getPersonID() != ID)
 			throw new IllegalArgumentException(
-				String.format("Student %d given entry by %d",
-				              ID, entry.getPersonID()));
+			    String.format("Student %d given entry by %d",
+				          ID, entry.getPersonID()) );
 		records.add(entry);
+		pushBack();
+		refreshCache();
 	}
 
 	/**
@@ -100,7 +114,6 @@ class Student{
 
 	/**
 	 * @return Number of valid peer interaction data points given by this Student.
-	 * @todo Implement.
 	 */
 	public int feedbackGiven(){
 		return records.size();
@@ -110,27 +123,190 @@ class Student{
 	 * @return The average rating given by this Student for all lectures 
 	 *         so far in the semester.
 	 */
-	public double ratingMean(){
+	public float ratingMean(){
+		if (!mutated)
+			return ratingMean;
 		if (records.size() == 0)
-			return Double.NaN;
+			return Float.NaN;
 		long sum = 0;
 		for (PeerInteraction elem : records)
 			sum += elem.getGrade();
-		return ((double) sum)/records.size();
+		return ratingMean = ((float) sum)/records.size();
 	}
 
 	/**
 	 * @return The standard deviation of all ratings given by this
 	 *         Student throughout the semester
 	 */
-	public double ratingStdDev(){
+	public float ratingStdDev(){
+		if (!mutated)
+			return ratingStdDev;
 		if (records.size() == 0)
-			return Double.NaN;
+			return Float.NaN;
 		long sumSquared = 0;
-		double ratingMean = ratingMean();
+		double mean = ratingMean();
 		for (PeerInteraction elem : records)
 			sumSquared += (elem.getGrade()*elem.getGrade());
-		double meanSquared = ((double) sumSquared)/records.size();
-		return Math.sqrt(meanSquared-ratingMean*ratingMean);
+		double meanSquared = ((float) sumSquared)/records.size();
+		return ratingStdDev = (float) Math.sqrt(meanSquared-mean*mean);
+	}
+	
+	/**
+	 * Ensures that the last few entries added are from distinct lectures.
+	 * If duplicates are found, all will be merged into a single entry.
+	 * This is public for now because it's unclear where it should be
+	 * called.
+	 */
+	public void mergeRecentDuplicates(){
+		int last = records.size()-1;
+		ArrayList<PeerInteraction> duplicates = new ArrayList<>();
+		while (last > 0){;
+			Lecture curr = Lecture.get(records.get(last));
+			Lecture prev = Lecture.get(records.get(last));
+			if (curr != prev)
+				break;
+			else
+				--last;
+		}
+		if (last != records.size()-1){
+			PeerInteraction[] repeats 
+			    = new PeerInteraction[records.size()-last];
+			for (int idx = 0; idx < records.size()-last; ++idx)
+				repeats[idx] = records.get(idx+last);
+			for (int idx = records.size()-1; idx >= last; ++idx)
+				records.remove(idx); //Shame Java has no removeLast
+			records.add(new PeerInteraction(repeats));
+			refreshCache(); //Recomputation needed due to changes
+		}
+	}
+
+	/**
+	 * Performs the same function as mergeRecentDuplicates(), but does
+	 * so for all entries added. This might be slower but also might
+	 * preferable due to lack of preconditions.
+	 */
+	public void mergeAllDuplicates(){
+		ArrayList<PeerInteraction> newRecords = new ArrayList<>();
+		ArrayList<PeerInteraction> bucket = new ArrayList<>();
+		Lecture last = null;
+		PeerInteraction[] merged = new PeerInteraction[1];
+		for (PeerInteraction entry : records){
+			Lecture curr = Lecture.get(entry);
+			if (curr != last && bucket.size() > 0){
+				merged = new PeerInteraction[bucket.size()];
+				bucket.toArray(merged);
+				newRecords.add(new PeerInteraction(merged));
+				bucket = new ArrayList<>();
+			}
+			bucket.add(entry);
+			last = curr;
+		}
+		if (bucket.size() > 0){
+			merged = new PeerInteraction[bucket.size()];
+			bucket.toArray(merged);
+			newRecords.add(new PeerInteraction(merged));
+		}
+		records = newRecords;
+	}
+	
+	/**
+	 * Private function that refreshes all cached variables. Setting
+	 * mutated to true causes all functions to recompute instead
+	 * of returning their cached values.
+	 */
+	private void refreshCache(){
+		mutated = true;
+		ratingMean();
+		ratingStdDev();
+		feedbackWeight();
+		mutated = false;
+	}
+	
+	/**
+	 * Private helper function that pushes back an unsorted entry to
+	 * its proper spot in Student.records. Called whenever an entry is
+	 * added.
+	 */
+	private void pushBack(){
+		int last = records.size()-1;
+		while (last > 0 
+		    && records.get(last-1).getDate().compareTo(
+		           records.get(last).getDate()) > 0){
+		//^ Java verbosity in a nutshell.
+		//  Translation: records[last-1].getDate() > records[last].getDate();
+			Collections.swap(records, last-1, last);
+			--last;
+		}
+	}
+
+
+	/**
+	 * Chronological iterator that points to the first element of
+	 * records.
+	 */
+	public InteractionIterator iterator() { return new InteractionIterator(); }
+
+	public class InteractionIterator implements Iterator<PeerInteraction>{
+		private int curr;
+		public InteractionIterator()     { curr = 0; }
+		public boolean hasNext()         { return curr < records.size();}
+		public PeerInteraction next()    { return records.get(curr++); }
+		public void remove() { throw new UnsupportedOperationException(); } 
+	}
+
+	
+	/**
+	 * For now, this test method only works for feedback files without
+	 * NRLists (so all netIDs must be integers). It also writes nothing
+	 * to any files. It mainly tests to see if mergeDuplicates works.
+	 * The parameters DUPLICATE and INVALID_ID are ratios that indicate
+	 * the probability of this test inserting duplicate entries and
+	 * trying to add entries with a mismatched ID (for testing).
+	 * 
+	 * @args The source file for PeerInteractions or nothing
+	 */
+	public static void main(String[] args){
+		LectureInitializer.initialize();
+		String fileName = args.length == 1 ? args[0] : null;
+		double DUPLICATE = 0.00;
+		double INVALID_ID = 0.15;
+		Scanner safeScan = new Scanner(System.in);
+		if (fileName == null){
+			System.out.print("Enter the name of a feedback source file: ");
+			fileName = safeScan.nextLine();
+			TextIO.readFile(fileName);
+		}
+		ArrayList<PeerInteraction> results = new ArrayList<PeerInteraction>();
+		while (!TextIO.eof()){
+			try{
+				PeerInteraction temp = new PeerInteraction(TextIO.getln());
+				results.add(temp);
+			}catch(Exception e) {/* Do nothing */}
+		}
+		PeerInteraction rand;
+		do{
+			rand = results.get((int) (results.size()*Math.random()));
+		}while(!rand.valid());
+		Student trial = new Student(rand); //This will lead to duplication
+		Collections.shuffle(results);     //Tests functionality of pushBack()
+		for (PeerInteraction elem : results){
+			if (elem.getPersonID() != trial.getID() &&
+			    Math.random() >= INVALID_ID)
+				continue;
+			try{
+				trial.addEntry(elem);
+				while (Math.random() < DUPLICATE)
+					trial.addEntry(elem);
+			}catch(IllegalArgumentException e){
+				System.out.printf("Caught bad add: %s\n", e.getMessage());
+			}
+		}
+		System.out.println("Data with possible duplicates:");
+		for(PeerInteraction elem : trial)
+			System.out.println(elem);
+		trial.mergeAllDuplicates();
+		System.out.println("\n\nData with duplicates merged:");
+		for (PeerInteraction elem : trial)
+			System.out.println(elem);
 	}
 }
