875bb7293d1847c97d00e91592ddbe5d0f3c08d5
Bliss Chapman <bliss@ag-informatics.com>
2016-01-04 18:22:38 -0600

    Fixed compiler errors by reverting the roster class back to what I think it should be.

--- src/Roster.java
+++ src/Roster.java
@@ -1,311 +1,224 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.RandomAccess;
-import java.util.Scanner;
-
 /**
- * Student class. Stores information regarding a particular Student in class, 
- * including their lecture attendance and possibly grades. Each Student
- * will contain all the proper PeerInteractions made by the student.
+ * A container which stores all Students in the class for processing. Implements
+ * a constant-time getter method which maps student ID codes to their
+ * corresponding Student objects. 
  *
  * @author CS125 Research
- * @todo Implement more.
+ * @todo Add useful features. Also implement Student class and add needed
+ *       functionality to NRList class.
  */
-class Student implements Iterable<PeerInteraction>{
-	public static final float WEIGHT_PROPORTIONALITY_CONSTANT = 1;
-	public static final float WEIGHT_THRESHOLD = 10;
-	
-	/* ID of this Student. This value is immutable. */
-	private final int ID;
-	
-	//private boolean female; //One possibility for what we could store here
-	
-	/* The collection of all PeerInteractions made by the Student, sorted
-	 * in chronological order. */
-	private ArrayList<PeerInteraction> records;
+import java.util.*;
+public class Roster implements Iterable<Student>{
 
-	/* Caching variables for several computationally nontrivial methods.
-	 * Stores results of these method calls and is recomputed whenever
-	 * the Student is mutated. */
-	private float weight = Float.NaN;
-	private float ratingMean = Float.NaN;
-	private float ratingStdDev = Float.NaN;
-	/* When the cache is to be refreshed, this boolean is set true. Any
-	 * methods that use the above variables will recompute values when
-	 * this boolean is set. See refreshCache()*/
-	private boolean mutated = false;
+	private Student[] map;
+	private int numStudents;
 	
- 	/**
- 	 * ID Ctor. Needs work?
- 	 * 
- 	 * @param code The ID of the Student.
- 	 */
-	public Student(int code){
-		ID = code;
-		records = new ArrayList<PeerInteraction>();
+	/**
+	 * Constructor for the Roster class. Creates an array of specified capacity
+	 * and keeps it empty. Students can be manually added later to the Roster
+	 * via the addStudent(int) method.
+	 */
+	public Roster(int capacity){
+		map = new Student[capacity];
+		numStudents = 0;
 	}
 	
 	/**
-	 * ID and one-entry Ctor. Is this useful?
+	 * Constructor for the Roster class. Creates an array of specified capacity
+	 * and fills it up with new Student objects. The Students are initialized
+	 * based off the ID codes in the NRList argument.
 	 * 
-	 * @param entry The first PeerInteraction made by this student.
+	 * @codes    An NRList containing all the encoded netIDs in the class roster.
+	 * @capacity The maximum number of students that can be added (can be changed).
 	 */
-	public Student(PeerInteraction entry){
-		this(entry.getPersonID());
-		records.add(entry);
-		refreshCache();
+	public Roster(NRList codes, int capacity){
+		this(capacity);
+		/* Goes through all the ID codes in the NRList argument and creates a
+		 * Student object for each one. The Students created are stored in an
+		 * array at the index corresponding to their ID codes. So map[154] would
+		 * correspond to the Student whose ID code is 154. That being said, we
+		 * should ensure that the range of Student ID codes is relatively small
+		 * or else the array will waste space.
+		 */
+		for (NetIDPair elem : codes){
+			map[elem.getCode()] = new Student(elem.getCode());
+			numStudents = codes.pairList.size();
+		}
 	}
 
 	/**
-	 * Computes and returns the weight of this Student, used when calculating
-	 * weighted grade distributions for a Lecture. 
+	 * Given an ID, returns the Student with the matching ID in the Roster. 
+	 * Returns null if the ID is not found. This method is guarranteed to
+	 * run in constant time.
 	 *
-	 * Formula: (2 * Number of records by student) / (Number of times the record had a feedback rating of 5 OR 10)
-	 * 	Weight can be a max of WEIGHT_THRESHOLD
+	 * @param ID The code associated with a particular student.
 	 *
-	 * @return The weight given to this Student's feedback.
+	 * @return The student associated with the given code or null.
 	 */
-	public double feedbackWeight(){
-		if (!mutated)
-			return WEIGHT_PROPORTIONALITY_CONSTANT*weight;
-		if (records.size() == 0)
-			return Double.NaN;
-		int numberOfCommonResponses = 0; // For now, if the feedback was 5 or 10, it's not important
-		for(int i = 0; i < records.size(); i++){
-			if(records.get(i).getGrade() == 5 || records.get(i).getGrade() == 10){
-				numberOfCommonResponses++;
-			}
-		}
-		if(numberOfCommonResponses > 0)
-			weight = (2*records.size())/numberOfCommonResponses;
+	public Student get(int ID){
+		if (ID >= map.length || ID < 0)
+			return null;
 		else
-			weight = WEIGHT_THRESHOLD;
-		
-		if(weight > WEIGHT_THRESHOLD)
-			weight = WEIGHT_THRESHOLD;
-		
-		return WEIGHT_PROPORTIONALITY_CONSTANT*weight;
-	}
-
-	/**
-	 * Add a PeerInteraction to this Student's record thereof. This method 
-	 * will sort the record by date after insertion. Throws an exception 
-	 * if an entry passed in has a personID which differs from this.ID .
-	 *
-	 * @param entry PeerInteraction made by this Student
-	 */
-	public void addEntry(PeerInteraction entry){
-		if (entry.getPersonID() != ID)
-			throw new IllegalArgumentException(
-			    String.format("Student %d given entry by %d",
-				          ID, entry.getPersonID()) );
-		records.add(entry);
-		pushBack();
-		refreshCache();
-	}
-
-	/**
-	 * @return The ID code of this student.
-	 */
-	public int getID(){
-		return ID;
-	}
-
-	/**
-	 * @return Number of valid peer interaction data points given by this Student.
-	 */
-	public int feedbackGiven(){
-		return records.size();
-	}
-
-	/**
-	 * @return The average rating given by this Student for all lectures 
-	 *         so far in the semester.
-	 */
-	public float ratingMean(){
-		if (!mutated)
-			return ratingMean;
-		if (records.size() == 0)
-			return Float.NaN;
-		long sum = 0;
-		for (PeerInteraction elem : records)
-			sum += elem.getGrade();
-		return ratingMean = ((float) sum)/records.size();
-	}
-
-	/**
-	 * @return The standard deviation of all ratings given by this
-	 *         Student throughout the semester
-	 */
-	public float ratingStdDev(){
-		if (!mutated)
-			return ratingStdDev;
-		if (records.size() == 0)
-			return Float.NaN;
-		long sumSquared = 0;
-		double mean = ratingMean();
-		for (PeerInteraction elem : records)
-			sumSquared += (elem.getGrade()*elem.getGrade());
-		double meanSquared = ((float) sumSquared)/records.size();
-		return ratingStdDev = (float) Math.sqrt(meanSquared-mean*mean);
+			return map[ID];
 	}
 	
 	/**
-	 * Ensures that the last few entries added are from distinct lectures.
-	 * If duplicates are found, all will be merged into a single entry.
-	 * This is public for now because it's unclear where it should be
-	 * called.
-	 */
-	public void mergeRecentDuplicates(){
-		int last = records.size()-1;
-		while (last > 0){;
-			Lecture curr = Lecture.get(records.get(last));
-			Lecture prev = Lecture.get(records.get(last));
-			if (curr != prev)
-				break;
-			else
-				--last;
-		}
-		if (last != records.size()-1){
-			PeerInteraction[] repeats 
-			    = new PeerInteraction[records.size()-last];
-			for (int idx = 0; idx < records.size()-last; ++idx)
-				repeats[idx] = records.get(idx+last);
-			for (int idx = records.size()-1; idx >= last; ++idx)
-				records.remove(idx); //Shame Java has no removeLast
-			records.add(new PeerInteraction(repeats));
-			refreshCache(); //Recomputation needed due to changes
-		}
-	}
-
-	/**
-	 * Performs the same function as mergeRecentDuplicates(), but does
-	 * so for all entries added. This might be slower but also might
-	 * preferable due to lack of preconditions.
-	 */
-	public void mergeAllDuplicates(){
-		ArrayList<PeerInteraction> newRecords = new ArrayList<>();
-		ArrayList<PeerInteraction> bucket = new ArrayList<>();
-		Lecture last = null;
-		PeerInteraction[] merged = new PeerInteraction[1];
-		for (PeerInteraction entry : records){
-			Lecture curr = Lecture.get(entry);
-			if (curr != last && bucket.size() > 0){
-				merged = new PeerInteraction[bucket.size()];
-				bucket.toArray(merged);
-				newRecords.add(new PeerInteraction(merged));
-				bucket = new ArrayList<>();
-			}
-			bucket.add(entry);
-			last = curr;
-		}
-		if (bucket.size() > 0){
-			merged = new PeerInteraction[bucket.size()];
-			bucket.toArray(merged);
-			newRecords.add(new PeerInteraction(merged));
-		}
-		records = newRecords;
-	}
-	
-	/**
-	 * Private function that refreshes all cached variables. Setting
-	 * mutated to true causes all functions to recompute instead
-	 * of returning their cached values.
-	 */
-	private void refreshCache(){
-		mutated = true;
-		ratingMean();
-		ratingStdDev();
-		feedbackWeight();
-		mutated = false;
-	}
-	
-	/**
-	 * Private helper function that pushes back an unsorted entry to
-	 * its proper spot in Student.records. Called whenever an entry is
-	 * added.
-	 */
-	private void pushBack(){
-		int last = records.size()-1;
-		while (last > 0 
-		    && records.get(last-1).getDate().compareTo(
-		           records.get(last).getDate()) > 0){
-		//^ Java verbosity in a nutshell.
-		//  Translation: records[last-1].getDate() > records[last].getDate();
-			Collections.swap(records, last-1, last);
-			--last;
-		}
-	}
-
-
-	/**
-	 * Chronological iterator that points to the first element of
-	 * records.
-	 */
-	public InteractionIterator iterator() { return new InteractionIterator(); }
-
-	public class InteractionIterator implements Iterator<PeerInteraction>{
-		private int curr;
-		public InteractionIterator()     { curr = 0; }
-		public boolean hasNext()         { return curr < records.size();}
-		public PeerInteraction next()    { return records.get(curr++); }
-		public void remove() { throw new UnsupportedOperationException(); } 
-	}
-
-	
-	/**
-	 * For now, this test method only works for feedback files without
-	 * NRLists (so all netIDs must be integers). It also writes nothing
-	 * to any files. It mainly tests to see if mergeDuplicates works.
-	 * The parameters DUPLICATE and INVALID_ID are ratios that indicate
-	 * the probability of this test inserting duplicate entries and
-	 * trying to add entries with a mismatched ID (for testing).
+	 * Given a PeerInteraction, returns the Student in the Roster whose ID
+	 * matches the first netID of the argument or null if no such Student
+	 * is found.
 	 * 
-	 * @args The source file for PeerInteractions or nothing
+	 * @param interaction The PeerInteraction whose author will be sought.
+	 * @return the student who authored the interaction or null.
+	 */
+	public Student get(PeerInteraction interaction){
+		return get(interaction.getPersonID());
+	}
+	
+	/**
+	 * This method matches all elements from a batch of PeerInteractions
+	 * to individual Students in this Roster and assigns those
+	 * PeerInteractions to their corresponding students using
+	 * Student.addEntry(PeerInteraction).
+	 * 
+	 * @param batch A collection of PeerInteractions to be added.
+	 */
+	public void addInteractions(Iterable<PeerInteraction> batch){
+		for (PeerInteraction entry : batch){
+			Student person = get(entry);
+			if (person != null)
+				person.addEntry(entry);
+		}
+		for (Student person : this)
+			person.mergeRecentDuplicates(); //All or Recent?
+	}
+	
+	/**
+	 * @return The number of students in the Roster.
+	 */
+	public int size()     { return numStudents; }
+	
+	/**
+	 * @return The number of students this Roster can contain.
+	 */
+	public int capacity() { return map.length; }
+	
+	/**
+	 * In case new Students join the class, allows addition of new Student to
+	 * Roster. Ensure extra space when constructing Roster or else this will
+	 * not be possible.
+	 */
+	public void addStudent(int ID){
+		if (ID < 0 || ID >= map.length)
+			throw new 
+			    IndexOutOfBoundsException(ID + " is out of Roster Range.");
+		if (map[ID] != null)
+			throw new IllegalArgumentException(ID + " is already taken.");
+		map[ID] = new Student(ID);
+		++numStudents;
+	}
+	 
+	/**
+	 * Resizes this Roster to allow addition of more Students. If shrinking
+	 * the Roster would cut off existing students, throws an exception.
+	 *
+	 * @param newSize New size of Roster.
+	 */
+	public void resize(int newSize){
+		int max = map.length;
+		while (map[--max] == null);
+		if (newSize < max)
+			throw new
+			    IndexOutOfBoundsException("Minimum capacity needed: " + max);
+		Student[] newMap = new Student[newSize];
+		for (int i = 0; i < map.length; ++i)
+			newMap[i] = map[i];
+		map = newMap;
+	}
+	
+	
+	/**
+	 * @return An iterator to the first Student in this Roster (in order of
+	 *         increasing ID)
+	 */
+	public StudentIterator iterator() { return new StudentIterator(); }
+
+	/**
+	 * Hashmap iterator. Jumps from one non-null element of map to the next.
+	 */
+	public class StudentIterator implements Iterator<Student>{
+		private int curr;
+		
+		/**
+		 * Helper function that jumps to the next non-null element
+		 * of map or until the end of map is reached.
+		 */
+		private void jump()      { while (hasNext() && map[++curr] == null); }
+		public StudentIterator() { curr = -1; jump(); }
+		public boolean hasNext() { return curr < map.length - 1; }
+		public Student next(){
+			Student out = map[curr];
+			jump();
+			return out;
+		}
+		@Override
+		public void remove() {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	
+	/**
+	 * Simple main function. Shows that the iterator for this class
+	 * works properly.
+	 * TODO improve to also test 
+	 *      Roster.addPeerInteractions(Iterable<PeerInteraction>)
 	 */
 	public static void main(String[] args){
-		LectureData.initialize();
-		String fileName = args.length == 1 ? args[0] : null;
-		double DUPLICATE = 0.00;
-		double INVALID_ID = 0.15;
-		Scanner safeScan = new Scanner(System.in);
-		if (fileName == null){
-			System.out.print("Enter the name of a feedback source file: ");
-			fileName = safeScan.nextLine();
-			TextIO.readFile(fileName);
+		int CAPACITY = 1000;
+		int SIZE = 200;
+		int DELTA = 500;
+		Roster testRoster = new Roster(CAPACITY);
+		ArrayList<Integer> oneToThousand = new ArrayList<>();
+		for (int i = 0; i < CAPACITY; ++i)
+			oneToThousand.add(i);
+		Collections.shuffle(oneToThousand);
+		int count = 0;
+		for (int i = 0; i < SIZE; ++i){
+			int number = oneToThousand.get(i);
+			testRoster.addStudent(number);
 		}
-		ArrayList<PeerInteraction> results = new ArrayList<PeerInteraction>();
-		while (!TextIO.eof()){
-			try{
-				PeerInteraction temp = new PeerInteraction(TextIO.getln());
-				results.add(temp);
-			}catch(Exception e) {/* Do nothing */}
+		//Test to see that iteration works as expected
+		for (Student person : testRoster){
+			++count;
+			System.out.printf("ID: %d\n", person.getID());
 		}
-		PeerInteraction rand;
-		do{
-			rand = results.get((int) (results.size()*Math.random()));
-		}while(!rand.valid());
-		Student trial = new Student(rand); //This will lead to duplication
-		Collections.shuffle(results);     //Tests functionality of pushBack()
-		for (PeerInteraction elem : results){
-			if (elem.getPersonID() != trial.getID() &&
-			    Math.random() >= INVALID_ID)
-				continue;
+		//Check to see that iterator reached all students
+		System.out.printf("Number of iterations: %d\n", count);
+		System.out.printf("Size of roster: %d\n\n", testRoster.size());
+		//Check that bounds exception is properly thrown
+		try{
+			//Randomly tries underflow or overflow by up to DELTA
+			int wildcard = Math.random() > 0.50 ? CAPACITY : -DELTA;
+			testRoster.addStudent(wildcard + (int)(Math.random()*DELTA));
+			System.out.println("Bounds check test failed.\n");
+		}catch(IndexOutOfBoundsException e){
+			System.out.println(e.getMessage());
+			System.out.println("Bounds check test successful.\n");
+		}
+		//After this resize, no bounds exceptions should be thrown.
+		//But the method should properly handle assignments to IDs
+		//that are already taken.
+		testRoster.resize(CAPACITY + DELTA);
+		for (int i = 0; i < CAPACITY + DELTA; ++i){
 			try{
-				trial.addEntry(elem);
-				while (Math.random() < DUPLICATE)
-					trial.addEntry(elem);
+				testRoster.addStudent(i);
+				System.out.printf("Added Student %d\n", i);
 			}catch(IllegalArgumentException e){
-				System.out.printf("Caught bad add: %s\n", e.getMessage());
+				System.out.println(e.getMessage());
 			}
 		}
-		System.out.println("Data with possible duplicates:");
-		for(PeerInteraction elem : trial)
-			System.out.println(elem);
-		trial.mergeAllDuplicates();
-		System.out.println("\n\nData with duplicates merged:");
-		for (PeerInteraction elem : trial)
-			System.out.println(elem);
+		
 	}
+
 }
