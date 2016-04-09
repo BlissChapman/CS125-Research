109c80b56fc6db3b717b5662e8f6dfc5fa2cccd3
Bliss Chapman <bliss@agrible.com>
2016-04-09 16:46:35 -0500

    Testing new JDK.

--- src/Main.java
+++ src/Main.java
@@ -7,20 +7,36 @@ import java.util.ArrayList;
 public class Main {
 
 	public static boolean buildSucceeded = false;
-	
+
 	public static void main(String[] args) {
+		// Anonymous Runnable
+		
+//		Runnable r1 = new Runnable() {
+//			@Override
+//			public void run(){
+//				System.out.println("Hello world one!");
+//			}
+//		};
+//		Runnable r2 = () -> System.out.println("Hello world two!");
+//
+//		// Run em!
+//		r1.run();
+//		r2.run();
+
+
+
 		PeerInteractionsData.initialize();
 		System.out.printf("Percentage of entries valid: %f%%\nNumber of valid entries: %d\nNumber of entries: %d\n", PeerInteractionsData.percentValid, PeerInteractionsData.numberOfValidEntries, PeerInteractionsData.numberOfEntries);
 		System.out.println("======================================================================================\n");
-		
+
 		LectureData.initialize();
-		
+
 		//Print each lecture object:
 		ArrayList<Lecture> lectures = LectureData.lectures;
 		for(int j = 0; j < lectures.size(); j++) {
 			System.out.println(lectures.get(j));
 		}
-		
+
 		//Print stats:
 		System.out.printf("Average: %f, Standard Deviation: %f\n", LectureData.mean, LectureData.stdDev);
 		System.out.println("Number of lectures: " + lectures.size());
