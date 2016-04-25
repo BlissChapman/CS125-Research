6310fe1b607aad516bc8607474a1bf828d185882
Bliss Chapman <bliss@agrible.com>
2016-04-25 08:19:49 -0500

    update gitignore to include .project file

--- src/JSONConvertible.java
+++ src/JSONConvertible.java
@@ -5,7 +5,7 @@
 public interface JSONConvertible {
 
 	/**
-	 * Returns a JSON string representation of class 
+	 * Creates a JSON string representation of a class 
 	 * instance.
 	 * 
 	 * @return A JSON string representation of this class instance.
